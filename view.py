9e8484d03b25a84e896e98ce9b30e69df0a95bf6
mariaBarczyk <barczyk.maria92@gmail.com>
2017-10-29 19:20:03 +0100

    Implement display task table

--- view.py
+++ view.py
@@ -1,11 +1,15 @@
-
-MENU_OPTIONS = ['Add task', 'Modify task', 'Remove task',
-                'Mark task', 'Display task list', 'Display task details']
+from prettytable import PrettyTable
 
 
 class View:
+
+    MENU_OPTIONS = ['Add task', 'Modify task', 'Remove task',
+                    'Mark task', 'Display task list', 'Display task details']
 
     def display_menu():
         print('Welcome in ToDo application')
         for option in MENU_OPTIONS:
             print(str(MENU_OPTIONS.index(option)) + ". " + option)
+
+    def display_all_tasks(self, table):
+        print(table)
