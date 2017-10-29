3bbafba0c5abab994241e85fbdc389a894f65b71
mariaBarczyk <barczyk.maria92@gmail.com>
2017-10-29 18:35:41 +0100

    implement modify and remove task

--- model.py
+++ model.py
@@ -17,3 +17,12 @@ class ToDoList:
     def add_task(self, name, description):
         task = ToDoTask(name, description)
         self.to_do_list.append(task)
+
+    def remove_task(self, task):
+        self.to_do_list.remove(task)
+
+    def modify_task_name(self, task, new_name):
+        task.name = new_name
+
+    def modify_task_description(self, task, new_description):
+        task.description = new_description
