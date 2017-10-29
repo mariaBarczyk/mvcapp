5b2a73415312c4f9e151394df8ede70266246ef9
mariaBarczyk <barczyk.maria92@gmail.com>
2017-10-29 22:58:20 +0100

    implement wrong inputs prints

--- view.py
+++ view.py
@@ -7,10 +7,17 @@ class View:
     def display_menu():
         print('Welcome in ToDo application')
         for option in MENU_OPTIONS:
-            print(str(MENU_OPTIONS.index(option)) + ". " + option)
+            print(str(MENU_OPTIONS.index(option) + 1) + ". " + option)
 
     def display_all_tasks(self, table):
         print(table)
 
-    def display_message(self):
-        print('Wrong input')
+    def display_task_details(self, task_details):
+        print(task_details)
+
+    def display_wrong_lenght_msg(self):
+        print('To long! name < 20 and description < 150 characters.')
+
+    @staticmethod
+    def display_wrong_task_name():
+        print('Wrong task name.')
