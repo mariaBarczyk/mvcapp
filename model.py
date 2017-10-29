d4b2b5247dee76eafe501d463e696e25aee6a9c8
mariaBarczyk <barczyk.maria92@gmail.com>
2017-10-29 18:31:06 +0100

    Create ToDoTask and ToDoList class

---
+++ model.py
@@ -0,0 +1,12 @@
+class ToDoTask:
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.is_done = is_done
+
+
+class ToDoList:
+
+    def __init__(self):
+        self.to_do_list = []
