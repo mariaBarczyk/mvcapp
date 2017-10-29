9bc889ed7294e1643147de36a05a3549e554a9fa
mariaBarczyk <barczyk.maria92@gmail.com>
2017-10-29 20:01:46 +0100

    Change place for menu options list

--- view.py
+++ view.py
@@ -1,7 +1,8 @@
-class View:
+MENU_OPTIONS = ['Add task', 'Modify task', 'Remove task',
+                'Mark task', 'Display task list', 'Display task details']
 
-    MENU_OPTIONS = ['Add task', 'Modify task', 'Remove task',
-                    'Mark task', 'Display task list', 'Display task details']
+
+class View:
 
     def display_menu():
         print('Welcome in ToDo application')
